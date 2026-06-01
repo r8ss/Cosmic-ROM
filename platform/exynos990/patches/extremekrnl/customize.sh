0e372237ad5eb0524bcc6b99a8a3770505b1f28a
redmax19 <alessandroaraujo0098@gmail.com>
2026-06-01 18:15:56 -0300

    Update customize.sh

--- platform/exynos990/patches/extremekrnl/customize.sh
+++ platform/exynos990/patches/extremekrnl/customize.sh
@@ -1,5 +1,5 @@
 # [
-EXTREMEKRNL_REPO="https://github.com/redmax19/android_kernel_samsung_exynos990"
+EXTREMEKRNL_REPO="https://github.com/Android-Artisan/android_kernel_samsung_exynos990"
 
 BUILD_KERNEL()
 {
